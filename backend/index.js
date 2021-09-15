261db5605f1a9d4ae8ef408e4c338501ae2042d1
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-15 19:48:27 +0300

    Add dotenv for environment variables

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 const app = express();
+mongoose.connect(process.env.MONGO_URl, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
 
 app.listen(5000, ()=>{
   console.log('Backend server running');
